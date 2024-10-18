661db1953045c250e108815d3b12061b4a3c31d3
Astroboy2013 <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-10-18 19:21:13 -0400

    Step 44
    d44

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Shape Objects")]
+    public GameObject[] shapePrefabs;
+    [Header("Deafault Spawn Delay Time")]
+    public float spawnDelay = 2f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 3f;
+    [Header("Game Over UI Object")]
+    public GameObject gameOverCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    void Spawn()
+    {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+    // Update is called once per frame
+    public void GameOver()
+    {
+        CancelInvoke("Spawn");
+        gameOverCanvas.SetActive(true);
+    }
+
+    
+    
+    void Update()
+    {
+
+    }
+}
